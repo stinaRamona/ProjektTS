3153a9d40f2b12d13697ac5c20a4941c7a16629b
Stina Persson <stpe1901@student.miun.se>
2024-05-14 11:07:53 +0200

    filtrering av kurser genom ämne fungerar

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,17 +2,20 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
   courseList: Course[] = []; 
   onlySubjects: String [] = []; 
+  selectedSubject: String = ''; 
+  subjectCourses: Course[] = [];
 
   constructor(private courseService: CourseService) {}
 
@@ -20,6 +23,7 @@ export class CoursesComponent {
     this.courseService.getCourses().subscribe(data => {
       this.courseList = data;
       this.getSubjects(); 
+      this.filterBySubject()
     })
   }
 
@@ -27,5 +31,19 @@ export class CoursesComponent {
   getSubjects(){
     const subjects = new Set(this.courseList.map(course => course.subject));
     this.onlySubjects = Array.from(subjects);
+  }
+
+  //För att visa kurser utvalda av ämne 
+  filterBySubject() {
+    if (this.selectedSubject === ''){
+      this.subjectCourses = this.courseList
+    } else {
+      this.subjectCourses = this.courseList.filter(course => course.subject === this.selectedSubject);
+      
+    }
+  }
+
+  onSubjectChange(){
+    this.filterBySubject(); 
   }
 }
